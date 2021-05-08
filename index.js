1085395513eb964e6c5207e7d3fb98d10a83b2de
GerardoRodvel <jose_gerardo_lpz@hotmail.com>
2021-05-08 18:42:52 -0500

    segunda prueba

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 app.use(express.static(__dirname + '/public/'));
 
-app.listen(process.env.PORT, ()=> {
+app.listen(process.env.PORT || 3000, ()=> {
 	console.log(`Servidor web escuchando en http://${process.env.HOST}:${process.env.PORT}`);
 });
 
